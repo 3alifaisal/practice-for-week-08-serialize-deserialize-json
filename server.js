eabef0d2ba313eb14d201d267f6d21106c2b1682
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-02-21 23:09:36 +0100

    finished practice

--- server.js
+++ server.js
@@ -10,20 +10,25 @@ const server = http.createServer((req, res) => {
 
   req.on("end", () => {
     // Parse the body of the request as JSON if Content-Type header is
-      // application/json
+    // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
-      // header is x-www-form-urlencoded
+    // header is x-www-form-urlencoded
     if (reqBody) {
-      req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
-        .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        }, {});
+      console.log(req.headers['content-type']);
 
+      if (req.headers['content-type'] === 'application/json') {
+        req.body = JSON.parse(reqBody);
+      } else if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        req.body = reqBody
+          .split("&")
+          .map((keyValuePair) => keyValuePair.split("="))
+          .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+          .map(([key, value]) => [key, decodeURIComponent(value)])
+          .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          }, {});
+      };
       // Log the body of the request to the terminal
       console.log(req.body);
     }
@@ -33,7 +38,16 @@ const server = http.createServer((req, res) => {
     };
 
     // Return the `resBody` object as JSON in the body of the response
+    
+    
+    res.setHeader('Content-type', 'application/json');
+   
+    res.body = JSON.stringify(resBody);
+    res.end(res.body);
+  
+    return;
   });
+
 });
 
 const port = 5000;
